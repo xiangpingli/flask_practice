21a1b13fe588037b56d18bab27595bc586d34be5
xiangpingli <lixiangping@aliyun.com>
2017-03-29 21:41:29 +0800

    upload hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask.ext.script import Manager
+
+app = Flask(__name__)
+manager = Manager(app)
+
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
+
+if __name__ == '__main__':
+    manager.run()
