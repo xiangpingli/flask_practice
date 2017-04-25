d5a0cfc26059121a6463a5bc937fbe113fb43a96
xiangpingli <lixiangping@aliyun.com>
2017-04-25 21:10:27 +0800

    update flask hello.py

--- hello.py
+++ hello.py
@@ -4,9 +4,13 @@ from flask.ext.script import Manager
 app = Flask(__name__)
 manager = Manager(app)
 
-@app.route('/')
+@app.route('/test/')
 def index():
     return '<h1>Hello World!</h1>'
+
+@app.route('/bad/')
+def badreq():
+    return '<h1>bad req!</h1>', 400
 
 if __name__ == '__main__':
     manager.run()
