87009f403b458f4e00ad124ec2f0fc890dd4507e
xiangpingli <lixiangping@aliyun.com>
2017-05-01 13:08:02 +0800

    update flask_bootscript

--- flask_bootstrap/hello.py
+++ flask_bootstrap/hello.py
@@ -24,5 +24,13 @@ def user():
 def index():
     return render_template("index.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     manager.run()
