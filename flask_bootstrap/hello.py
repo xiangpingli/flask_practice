f66a3e7644f32e5634673f652610ba572a41d599
xiangpingli <lixiangping@aliyun.com>
2017-05-01 12:55:18 +0800

    update flask_bootstrap test

--- flask_bootstrap/hello.py
+++ flask_bootstrap/hello.py
@@ -1,24 +1,28 @@
 from flask import Flask
 from flask import render_template
-from flask.ext.bootstrap import Bootstrap
+from flask.ext.script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+manager = Manager(app)
 bootstrap = Bootstrap(app)
 
 @app.route('/test/')
-def index():
+def test():
     return render_template('test.html')
+
+@app.route('/bad/')
+def badreq():
+    return render_template('bad.html')
 
 @app.route('/user/')
 @app.route('/user/<name>')
 def user():
     return render_template('user.html')
 
-@app.route('/bad/')
-def badreq():
-    return render_template('bad.html')
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",
-            port=5000,
-            debug=True)
+    manager.run()
