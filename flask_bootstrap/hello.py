2e555946bf6776305970a9732e886e3fd645e554
xiangpingli <lixiangping@aliyun.com>
2018-02-13 00:17:05 +0800

    update flask_bootstrap chapt 4 end

--- flask_bootstrap/hello.py
+++ flask_bootstrap/hello.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask import Flask, render_template, session, redirect, url_for, flash
 
 
 class NameForm(FlaskForm):
@@ -35,12 +36,14 @@ def user():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template("index.html", form=form, name=name)
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name']= form.name.data
+        return redirect(url_for('index'))
+    return render_template("index.html", form=form, name=session.get('name'))
 
 @app.errorhandler(404)
 def page_not_found(e):
